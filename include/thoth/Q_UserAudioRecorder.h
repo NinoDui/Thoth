18cfc6235c5fc74f6d3a2eef7ed1a6758d1a8128
Yue XU <xuyue.yolanda@gmail.com>
2026-02-01 23:31:55 +0800

    [FEAT] Add AudioRecorder

---
+++ include/thoth/Q_UserAudioRecorder.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <QAudioSource>
+#include <QFile>
+#include <QMediaDevices>
+#include <QObject>
+#include <filesystem>
+#include <memory>
+#include <optional>
+
+struct WAVHeader;
+
+class Q_UserAudioRecorder : public QObject {
+    Q_OBJECT
+   public:
+    explicit Q_UserAudioRecorder(QObject* parent = nullptr);
+    ~Q_UserAudioRecorder();
+
+    bool startRecording(const std::filesystem::path& filePath);
+    void stopRecording();
+    bool isRecording() const;
+
+    std::optional<std::filesystem::path> lastRecordingFilePath() const;
+
+   signals:
+    void updateAmplitude(float level);
+    void errorOccurred(const QString& errorMessage);
+
+   private slots:
+    void onReadyRecord();
+
+   private:
+    void initAudio();
+    float calculateRMS(const QByteArray& buffer);
+
+    std::unique_ptr<QAudioSource> m_audioSource;
+    std::unique_ptr<QFile> m_audioFile;
+
+    // observer pointer maintained by m_audioSource
+    QIODevice* m_audioStream = nullptr;
+
+    QAudioFormat m_audioFormat;
+    uint32_t m_totalBytes = 0;
+};
---
+++ src/core/Entity.cpp
@@ -0,0 +1,14 @@
+#include "internal/Entity.h"
+
+WAVHeader WAVHeader::create(uint32_t sampleRate, uint16_t numChannels, uint16_t bitsPerSample,
+                            uint32_t dataSize) {
+    WAVHeader h;
+    h.numChannels = numChannels;
+    h.sampleRate = sampleRate;
+    h.bitsPerSample = bitsPerSample;
+    h.dataSize = dataSize;
+    h.blockAlign = (numChannels * bitsPerSample) / 8;
+    h.byteRate = sampleRate * h.blockAlign;
+    h.overallSize = h.dataSize + 44 - 8;
+    return h;
+}
